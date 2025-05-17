7b099f23288a2b4970a749faf8022f4232e56046
Yanto <yanto.unix@gmail.com>
2025-05-17 21:03:32 +0700

    Implementasi navigasi tombol Mulai ke Login_Activity

--- app/src/main/java/com/view/pintarmath/Loading_Activity.kt
+++ app/src/main/java/com/view/pintarmath/Loading_Activity.kt
@@ -1,6 +1,8 @@
 package com.view.pintarmath
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class Loading_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_loading)
+
+        val btn_mulai = findViewById<Button>(R.id.btn_mulai)
+        btn_mulai.setOnClickListener {
+            val intent = Intent(this, Login_Activity2::class.java)
+            startActivity(intent)
+            finish()
+
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
