a9ed7705a107eae22525fde712dc9c5ebc03b9dd
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-05-25 20:58:05 +0700

    mengganti ID di kode kotlin

--- app/src/main/java/com/view/pintarmath/Login_Activity2.kt
+++ app/src/main/java/com/view/pintarmath/Login_Activity2.kt
@@ -11,7 +11,8 @@ class Login_Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/view/pintarmath/Register_Activity.kt
+++ app/src/main/java/com/view/pintarmath/Register_Activity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.TextView
+
+
+
 
 class Register_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val loginLink = findViewById<TextView>(R.id.textView8)
+        loginLink.setOnClickListener {
+            val intent = Intent(this, Login_Activity2::class.java)
+            startActivity(intent)
         }
     }
 }
