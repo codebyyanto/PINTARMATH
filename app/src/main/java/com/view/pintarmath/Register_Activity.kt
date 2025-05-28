ad3cfa4437158c460690f44a8dfab6c6dfc7a607
fatimahlailatulazzahra <afatim915@gmail.com>
2025-05-28 23:39:08 +0700

    menambahkan event untuk textview "Login" pada Register_Activity.kt

--- app/src/main/java/com/view/pintarmath/Register_Activity.kt
+++ app/src/main/java/com/view/pintarmath/Register_Activity.kt
@@ -1,6 +1,8 @@
 package com.view.pintarmath
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ class Register_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        // Event klik untuk mengarahkan ke Login_Activity2 saat TextView "Login" diklik
+        val klik_login = findViewById<TextView>(R.id.klik_login)
+        klik_login.setOnClickListener {
+            val intent = Intent(this, Login_Activity2::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
