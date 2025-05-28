66eb892dc67b4080aa7980d8e812c761819dd4d8
codebyyanto <yanto.unix@gmail.com>
2025-05-28 22:53:57 +0700

    menambahkan clean code pada Lupa_Password_Activity dan Loading_Activity

--- app/src/main/java/com/view/pintarmath/Lupa_Password_Activity.kt
+++ app/src/main/java/com/view/pintarmath/Lupa_Password_Activity.kt
@@ -1,16 +1,24 @@
 package com.view.pintarmath
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Lupa_Password_Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lupa_password)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
