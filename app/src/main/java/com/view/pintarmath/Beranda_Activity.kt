403aff469d43ccf9e7b45d860ca29e661f1c9c16
codebyyanto <yanto.unix@gmail.com>
2025-06-15 01:52:47 +0700

    menambakan event klik icon home,setting, profil pada Beranda_Activity

--- app/src/main/java/com/view/pintarmath/Beranda_Activity.kt
+++ app/src/main/java/com/view/pintarmath/Beranda_Activity.kt
@@ -1,6 +1,8 @@
 package com.view.pintarmath
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,27 @@ class Beranda_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_beranda)
+
+        // Inisialisasi ID
+        val homeIcon = findViewById<ImageView>(R.id.ic_home)
+        val settingIcon = findViewById<ImageView>(R.id.ic_setting)
+        val profilIcon = findViewById<ImageView>(R.id.ic_profil)
+
+        // Event Klik
+        homeIcon.setOnClickListener {
+            startActivity(Intent(this, Beranda_Activity::class.java))
+        }
+
+        settingIcon.setOnClickListener {
+            startActivity(Intent(this, Setting_Activity::class.java))
+        }
+
+
+        profilIcon.setOnClickListener {
+            startActivity(Intent(this, Profil_Activity::class.java))
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
