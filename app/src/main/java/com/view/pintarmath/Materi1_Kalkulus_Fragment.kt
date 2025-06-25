db0bcf330db7e98dd8abbf372cc7d4bde4ab83d9
codebyyanto <yanto.unix@gmail.com>
2025-06-26 04:20:45 +0700

    menambbkan fungsi tombol kembali ke Kalkulus fragment

--- app/src/main/java/com/view/pintarmath/Materi1_Kalkulus_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Materi1_Kalkulus_Fragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class Materi1_Kalkulus_Fragment : Fragment() {
@@ -12,6 +13,16 @@ class Materi1_Kalkulus_Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_materi1__kalkulus, container, false)
+        val view = inflater.inflate(R.layout.fragment_materi1__kalkulus, container, false)
+
+        // Tombol kembali
+        val backButton = view.findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Kalkulus_Fragment())
+                .commit()
+        }
+
+        return view
     }
 }
