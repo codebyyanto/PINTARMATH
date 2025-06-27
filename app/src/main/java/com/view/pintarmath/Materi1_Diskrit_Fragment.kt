026f6d8cb6baff0d73f1c26205cd5ab074ea68d6
fatimahlailatulazzahra <afatim915@gmail.com>
2025-06-27 23:44:19 +0700

    manambahkan fungsi Createview agar fragment materi 1 diskrit bisa tampil

--- app/src/main/java/com/view/pintarmath/Materi1_Diskrit_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Materi1_Diskrit_Fragment.kt
@@ -8,4 +8,12 @@ import android.view.ViewGroup
 
 class Materi1_Diskrit_Fragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_materi1__kalkulus, container, false)
+
+        return view
+    }
 }
