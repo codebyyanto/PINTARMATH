3e95d01c09cc8a41d5ecfd7101eeaa0a7e7046f7
fatimahlailatulazzahra <afatim915@gmail.com>
2025-06-28 00:17:23 +0700

    menambahkan fungsi button back di Materi2_Diskrit_Fragment agar kembali ke tampilan menu materi diskrit

--- app/src/main/java/com/view/pintarmath/Materi2_Diskrit_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Materi2_Diskrit_Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class Materi2_Diskrit_Fragment : Fragment() {
 
@@ -14,6 +15,14 @@ class Materi2_Diskrit_Fragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_materi2_diskrit, container, false)
 
+
+        // Tombol kembali
+        val backButton = view.findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Diskrit_Fragment())
+                .commit()
+        }
         return view
     }
 }
