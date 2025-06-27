ca0af257053c28bd2229ce1e7334d142cd228cfc
fatimahlailatulazzahra <afatim915@gmail.com>
2025-06-27 22:17:29 +0700

    menambahkan fungsi Crateview di Setting_Fragment.kt agar menu setting dapat tampil

--- app/src/main/java/com/view/pintarmath/Setting_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Setting_Fragment.kt
@@ -9,5 +9,10 @@ import android.view.ViewGroup
 
 
 class Setting_Fragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
 }
