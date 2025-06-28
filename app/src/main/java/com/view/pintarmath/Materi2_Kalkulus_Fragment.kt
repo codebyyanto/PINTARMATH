fdcc18d6ef631fc73236db165111b12729f15f6c
codebyyanto <yanto.unix@gmail.com>
2025-06-28 22:18:33 +0700

    menambahkan class baru untuk fragment materi ke 2 kalkulus

---
+++ app/src/main/java/com/view/pintarmath/Materi2_Kalkulus_Fragment.kt
@@ -0,0 +1,11 @@
+package com.view.pintarmath
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Materi2_Kalkulus_Fragment : Fragment() {
+
+}
