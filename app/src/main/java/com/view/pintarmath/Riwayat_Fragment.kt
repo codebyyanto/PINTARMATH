aaecd02c93889fc7e54fcfa95017670981491509
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-06-28 20:39:56 +0700

    manambahkan fungsi crateonview agar fragment_riwayat dapat tampil

--- app/src/main/java/com/view/pintarmath/Riwayat_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Riwayat_Fragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlin.contracts.Returns
 
 class Riwayat_Fragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_riwayat, container, false)
+
+        return view
+    }
 }
