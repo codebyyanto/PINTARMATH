14964e8523c70af0fbad267e08936ecb9d76f366
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-06-28 21:31:49 +0700

    manambahkan fungsi button materi 1 agar saat di klik menampikan fragment materi 1 aljabar

--- app/src/main/java/com/view/pintarmath/Aljabar_Fragment.kt
+++ app/src/main/java/com/view/pintarmath/Aljabar_Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 
 class Aljabar_Fragment : Fragment() {
@@ -24,6 +25,17 @@ class Aljabar_Fragment : Fragment() {
             // Ganti fragment kembali ke Beranda
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, Beranda_Fragment())
+                .commit()
+        }
+
+        //Materi 1
+        val btnMateri1 = view.findViewById<Button>(R.id.btn_materi1)
+
+        btnMateri1.setOnClickListener {
+            // Navigasi ke Materi1_Aljabar_Fragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Materi1_Aljabar_Fragment())
+                .addToBackStack(null)
                 .commit()
         }
 
